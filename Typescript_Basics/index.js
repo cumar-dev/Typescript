8f4d7211e31872eb1c9d3ce5355146e2b043dda4
cumar-dev <your_email@example.com>
2026-07-22 17:45:38 +0300

    today i learned function using typescript

--- Typescript_Basics/index.js
+++ Typescript_Basics/index.js
@@ -1,26 +1,47 @@
 "use strict";
-const userName = "john doe";
-console.log(`Hello ${userName}`);
-let number1 = 10;
-let number2 = 20;
-console.log(number1 + number2);
-let myName = "cumar acbdi axmed";
-console.log(`Hello ${myName}`);
-myName = "show man";
-console.log(myName);
-let age = 20;
-console.log(age);
-let isStudent = true;
-console.log(isStudent);
+// const userName: string = "john doe";
+// console.log(`Hello ${userName}`);
+// let number1: number = 10;
+// let number2: number = 20;
+// console.log(number1 + number2);
+// let myName: string = "cumar acbdi axmed";
+// console.log(`Hello ${myName}`);
+// myName = "show man";
+// console.log(myName);
+// let age: number = 20;
+// console.log(age);
+// let isStudent: boolean = true;
+// console.log(isStudent);
 // Array
-let users = ["Aamina", "canab", "iqro"];
-console.log(users);
-users.push("maryan", "nafiso");
-console.log(users[0]);
-let numbers = [100, 200, 300, 400];
-console.log(numbers);
+// let users = ["Aamina", "canab", "iqro"];
+// console.log(users);
+// users.push("maryan", "nafiso");
+// console.log(users[0]);
+// let numbers: Array<number> = [100, 200, 300, 400];
+// console.log(numbers);
 // tuple
-let entry = [1, "omar"];
-console.log(entry);
-entry.push(2, "cadnaan");
-console.log(entry);
+// let entry: [number, string] = [1, "omar"];
+// console.log(entry);
+// entry.push(2, "cadnaan");
+// console.log(entry);
+// function
+function greet(name) {
+    return "hello " + name;
+}
+console.log(greet("omar"));
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 4));
+function log(message, userId) {
+    console.log(`[${userId || "unknown"}] ${message}`);
+}
+log("hello");
+function sum(...numbers) {
+    return numbers.reduce((a, b) => a + b, 0);
+}
+sum(1, 2, 3);
+function arraysOfNames(...names) {
+    return names.find(name => name === "faadumo");
+}
+console.log(arraysOfNames("Ali", "Ahmed", "faadumo"));
--- Typescript_Basics/index.ts
+++ Typescript_Basics/index.ts
@@ -1,38 +1,69 @@
-const userName: string = "john doe";
-console.log(`Hello ${userName}`);
+// const userName: string = "john doe";
+// console.log(`Hello ${userName}`);
 
-let number1: number = 10;
-let number2: number = 20;
-console.log(number1 + number2);
+// let number1: number = 10;
+// let number2: number = 20;
+// console.log(number1 + number2);
 
-let myName: string = "cumar acbdi axmed";
-console.log(`Hello ${myName}`);
+// let myName: string = "cumar acbdi axmed";
+// console.log(`Hello ${myName}`);
 
-myName = "show man";
-console.log(myName);
+// myName = "show man";
+// console.log(myName);
 
-let age: number = 20;
-console.log(age);
+// let age: number = 20;
+// console.log(age);
 
-let isStudent: boolean = true;
-console.log(isStudent);
+// let isStudent: boolean = true;
+// console.log(isStudent);
 
 // Array
 
-let users = ["Aamina", "canab", "iqro"];
-console.log(users); 
+// let users = ["Aamina", "canab", "iqro"];
+// console.log(users);
 
-users.push("maryan", "nafiso");
+// users.push("maryan", "nafiso");
 
-console.log(users[0]);
+// console.log(users[0]);
 
-let numbers: Array<number> = [100, 200, 300, 400];
-console.log(numbers);
+// let numbers: Array<number> = [100, 200, 300, 400];
+// console.log(numbers);
 
 // tuple
 
+// let entry: [number, string] = [1, "omar"];
+// console.log(entry);
+// entry.push(2, "cadnaan");
+// console.log(entry);
 
-let entry: [number, string] = [1, "omar"];
-console.log(entry);
-entry.push(2, "cadnaan");
-console.log(entry);
+// function
+function greet(name: string): string {
+  return "hello " + name;
+}
+
+console.log(greet("omar"));
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(add(2, 4));
+
+function log(message: string, userId?: number) {
+  console.log(`[${userId || "unknown"}] ${message}`);
+}
+
+log("hello");
+
+
+function sum(...numbers: number[]): number {
+  return numbers.reduce((a, b) => a + b, 0);
+}
+
+sum(1, 2, 3);
+
+function arraysOfNames(...names: string[]): string | undefined {
+    return names.find(name => name === "faadumo");
+}
+
+console.log(arraysOfNames("Ali", "Ahmed", "faadumo"));
